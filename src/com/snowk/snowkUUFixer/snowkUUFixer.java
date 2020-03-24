55ad0e0c792596a5c3f4e19851fcf474ec3cb558
i43052739 <jasper.fang@qq.com>
2020-03-24 11:55:33 +0800

    version1.1

--- src/com/snowk/snowkUUFixer/snowkUUFixer.java
+++ src/com/snowk/snowkUUFixer/snowkUUFixer.java
@@ -1,8 +1,9 @@
-package com.snowk.SnowkUUFixer;
+package com.snowk.snowkUUFixer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.snowk.snowkUUFixer.listener.CustomInvListener;
+import com.snowk.snowkUUFixer.listener.InvListener;
+import com.snowk.snowkUUFixer.listener.LimitBlockPlace;
 
 
 public class SnowkUUFixer extends JavaPlugin{
@@ -20,7 +21,8 @@ public class SnowkUUFixer extends JavaPlugin{
     
     
     public void registerListeners() {
-        Bukkit.getPluginManager().registerEvents(new CustomInvListener(), this);
+        Bukkit.getPluginManager().registerEvents(new InvListener(), this);
+        Bukkit.getPluginManager().registerEvents(new LimitBlockPlace(), this);
     }
 
     @Override
